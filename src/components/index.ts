48dce8061e2aae653c209dac23952b6a28d53fd2
Javier Andres <79766563+dresandev@users.noreply.github.com>
2023-03-17 03:03:12 -0500

    interface redesign, removal of assets

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,19 @@
 import MenuButton from './MenuButton.astro'
 import Logo from './Logo.astro'
 import Header from './Header.astro'
+import Main from './Main.astro'
+import Presentation from './Presentation.astro'
+import Skills from './Skills.astro'
+import Projects from './Projects.astro'
+import Contact from './Contact.astro'
 
 export {
   MenuButton,
   Logo,
-  Header
+  Header,
+  Presentation,
+  Main,
+  Skills,
+  Projects,
+  Contact
 }
--- src/data/technologies.ts
+++ src/data/technologies.ts
@@ -1,1 +1,60 @@
-export { }
+const technologies = [
+  {
+    name: 'HTML',
+    color: '#E34C26'
+  },
+  {
+    name: 'REACT',
+    color: '#61DAFB'
+  },
+  {
+    name: 'TAILWIND',
+    color: '#06B6D4'
+  },
+  {
+    name: 'GIT',
+    color: '#F05032'
+  },
+  {
+    name: 'FLUTTER',
+    color: '#1DB5E0'
+  },
+  {
+    name: 'DART',
+    color: '#0175C2'
+  },
+  {
+    name: 'JAVASCRIPT',
+    color: '#F0DB4F'
+  },
+  {
+    name: 'MONGODB',
+    color: '#47A248'
+  },
+  {
+    name: 'CSS',
+    color: '#264DE4'
+  },
+  {
+    name: 'TYPESCRIPT',
+    color: '#007ACC'
+  },
+  {
+    name: 'SASS',
+    color: '#CC6699'
+  },
+  {
+    name: 'NODE JS',
+    color: '#8CC84B'
+  },
+  {
+    name: 'NEXT JS',
+    color: '#FFFFFF'
+  },
+  {
+    name: 'LEARNING...',
+    color: '#00934C'
+  }
+]
+
+export default technologies
